6fb0c0db85461afa4f2d15f62221f680edd8d82a
Jason Sznol <jason@timehop.com>
2020-11-28 16:12:58 -0500

    Removed inline modifiers from buildSrc to enable buildSrc compilation avoidance

--- buildSrc/src/main/kotlin/PropertyExtensions.kt
+++ buildSrc/src/main/kotlin/PropertyExtensions.kt
@@ -2,9 +2,9 @@ import org.gradle.api.Project
 
 fun Project.intProperty(propName: String) = Integer.parseInt(property(propName) as String)
 
-inline val Project.compileSdk: Int get() = intProperty("android.sdk.compile")
-inline val Project.minSdk: Int get() = intProperty("android.sdk.min")
-inline val Project.targetSdk: Int get() = intProperty("android.sdk.target")
+val Project.compileSdk: Int get() = intProperty("android.sdk.compile")
+val Project.minSdk: Int get() = intProperty("android.sdk.min")
+val Project.targetSdk: Int get() = intProperty("android.sdk.target")
 
 fun Project.androidx(library: String) =
     "androidx.$library:$library:${property("androidx.$library")}"
