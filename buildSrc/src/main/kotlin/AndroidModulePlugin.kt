1bb382352602150408a6a1181239fa844ed833fe
Jason Sznol <jason@timehop.com>
2021-01-23 19:59:02 -0500

    Downgraded Android Gradle Plugin to 4.1.2

--- buildSrc/src/main/kotlin/AndroidModulePlugin.kt
+++ buildSrc/src/main/kotlin/AndroidModulePlugin.kt
@@ -1,8 +1,6 @@
 @file:Suppress("UnstableApiUsage")
 
 import com.android.build.api.dsl.CommonExtension
-import com.android.build.api.extension.AndroidComponentsExtension
-import com.android.build.api.variant.AndroidVersion
 import org.gradle.api.*
 import org.gradle.kotlin.dsl.getByType
 
@@ -15,25 +13,19 @@ val Project.targetSdk: Int get() = intProperty("android.sdk.target")
 fun Project.androidx(library: String) =
     "androidx.$library:$library:${property("androidx.$library")}"
 
-internal val Project.androidComponents: AndroidComponentsExtension<*, *>
-    get() = extensions.getByType(AndroidComponentsExtension::class)
-
 internal val Project.commonExtension: CommonExtension<*,*,*,*,*,*,*,*>
     get() = extensions.getByType(CommonExtension::class)
 
 class AndroidModulePlugin: Plugin<Project> {
 
     override fun apply(project: Project) {
-        project.androidComponents.beforeVariants {
-            it.minSdkVersion = AndroidVersion(project.minSdk)
-        }
-
         project.commonExtension.apply {
             compileSdk = project.compileSdk
             compileOptions {
                 sourceCompatibility(JavaVersion.VERSION_1_8)
                 targetCompatibility(JavaVersion.VERSION_1_8)
             }
+            defaultConfig.minSdk = project.minSdk
         }
     }
 }
